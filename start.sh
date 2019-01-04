1644286e5eeee306d31c3932e25da9c24516db62
v_wbgyang <v_wbgyang@www.demo.com>
2019-01-04 17:29:55 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [ -f "dist/logs/all.log" ];then
-  rm -rf dist/logs/*
-fi
- touch dist/logs/all.log
 nohup java -cp dist/app/*:dist/conf/:dist/lib/* com.webank.demo.server.SampleApp &
 tail -f dist/logs/all.log
